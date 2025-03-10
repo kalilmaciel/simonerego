2cbe3e53b5765cd7f33bcb6bf0fb9b7ec4556f7e
Kalil Maciel <kalilmaciel@gmail.com>
2025-03-10 08:46:45 -0300

    Primeira versão final com mobile

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+
+var dataCacheName = 'simonerego.v1';
+var cacheName = 'simonerego.v1';
+var filesToCache = [];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+});
+
+self.addEventListener('push', function (event) {
+
+});
